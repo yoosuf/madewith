1c49cec79ee75a91247adcb67523c32fd5b10a15
Yoosuf <mayoosuf@gmail.com>
2016-11-15 07:41:19 +0530

    added laradoc and controllers

---
+++ app/Http/Controllers/Api/V1/CategooriesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class CategooriesController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    //
+}
